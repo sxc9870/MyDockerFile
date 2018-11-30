bd553fbb3c6f32178601cf996796ed00859c4cb7
shixuechen <35631371@qq.com>
2018-11-30 12:49:40 +0800

    sxc

---
+++ redis/build-sentinel.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+import os
+
+
+def buildeMode(argv):
+    password = argv[1]#.input("请输入主节点密码：")  # 节点认证密码
+    masterIp =argv[2]  #input("从节点时指定主节点IP和端口(:分隔)：")  # 主节点iP端口 与哨兵同一个IP
+    masterPort = masterIp.split(":")[1]  # 主节点端口
+    masterIp = masterIp.split(":")[0]
+    mk_server="##"
+    mk_sentinel=""
+    sentinelConf = ""
+    with open('src/sentinel.conf', 'r') as f:
+            sentinelConf = f.read()
+            sentinelConf = (sentinelConf % {'password': password, 'sName': "myMaster", 'sIp': masterIp,
+                                                'sPort': masterPort})
+
+    with open('target/sentinel.conf', 'w') as f:
+            f.write(sentinelConf)
+    with open('src/dockerfile', 'r') as f:
+        with open('target/dockerfile', 'w') as f2:
+            f2.write(f.read() % {"mk-server":mk_server,"mk-sentinel":mk_sentinel})
+    cmd = ""  # build命令,启动命令
+
+    cmd = "cd target && docker build -t redis:sentinel  ."
+
+    os.system(cmd)
+
+
+
+if __name__ == "__main__":
+
+    buildeMode(sys.argv)
