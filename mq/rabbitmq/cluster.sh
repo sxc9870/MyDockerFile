61b43bdc33ab5cf0689daf9c3d76622f19ead271
sxc9870 <35631371@qq.com>
2019-06-30 13:00:18 +0800

    sxc

--- mq/rabbitmq/cluster.sh
+++ mq/rabbitmq/cluster.sh
@@ -1,4 +1,15 @@
+docker exec -it mq2 /bin/bash
 rabbitmqctl stop_app
 rabbitmqctl reset
 rabbitmqctl join_cluster rabbit@mq1
 rabbitmqctl start_app
+exit
+
+
+docker exec -it mq3 /bin/bash
+rabbitmqctl stop_app
+rabbitmqctl reset
+rabbitmqctl join_cluster rabbit@mq1
+rabbitmqctl start_app
+exit
+
